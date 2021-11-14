83d335f1300a8e268be113f053b61205f938b7a9
Paulo <paulo.wueliton@hotmail.com>
2021-11-14 09:22:57 -0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Paulo Wueliton on 14/11/21.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
